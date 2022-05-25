cc194ac47ade01b9fcc939ffd6acb83a01d8999a
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-05-25 11:02:23 +0100

    header file

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,46 +1,24 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
-* recurse_list - Compare 1st and last node, then 2nd last and 2nd, etc
-*
-* @left: Keeps track of left side of list
-*
-* @right: Keeps track of nodes on right
-*
-* Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
-
-int recurse_list(listint_t **left, listint_t *right)
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
-	int result;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-	/* Base case for empty list*/
-	if (right == NULL)
-	{
-		return (1);
-	}
+int is_palindrome(listint_t **head);
 
-	/* Check values at left and right then 2nd node & 2nd to last, etc */
-	result = recurse_list(left, right->next) && (*left)->n == right->n;
-
-	/* Point left to next node */
-	*left = (*left)->next;
-
-	return (result);
-}
-
-/**
-* is_palindrome - Check if LL is a palindrome
-*
-* @head: Double pointer to head of list
-*
-* Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
-
-int is_palindrome(listint_t **head)
-{
-	listint_t *deref = *head;
-
-	return (recurse_list(&deref, deref));
-}
+#endif
