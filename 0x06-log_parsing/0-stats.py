70d199eb44d49aa68d276e6b4d182dc131d11e64
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-06-23 15:42:46 +0100

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,36 +3,30 @@
 read stdin line by line and computes metrics
 """
 import sys
-
-total_size = 0
-counter = 0
-
-dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
-                      '403': 0, '404': 0, '405': 0, '500': 0}
-
+STATUS = {'200': 0, '301': 0,
+          '400': 0, '401': 0,
+          '403': 0, '404': 0,
+          '405': 0, '500': 0}
+size = 0
+lines = 0
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 2:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in dict_codes_counter.keys():
-                dict_codes_counter[code] += 1
-            total_size += size
-            counter += 1
-
-        if counter == 10:
-            print("File size: {:d}".format(total_size))
-            for k, v in sorted(dict_codes_counter.items()):
-                if v != 0:
-                    print("{}: {:d}".format(k, v))
-            counter = 0
-
-except Exception:
-    pass
-
+    for argument in sys.stdin:
+        argumentList = argument.split(" ")
+        if len(argumentList) > 2:
+            status = argumentList[-2]
+            fileSize = int(argumentList[-1])
+            if status in STATUS:
+                STATUS[status] += 1
+            size += fileSize
+            lines += 1
+            if lines == 10:
+                print("File size: {:d}".format(size))
+                for key, value in sorted(STATUS.items()):
+                    if value != 0:
+                        print("{}: {:d}".format(key, value))
+                lines = 0
 finally:
-    print("File size: {}".format(total_size))
-    for k, v in sorted(dict_codes_counter.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    print("File size: {:d}".format(size))
+    for key, value in sorted(STATUS.items()):
+        if value != 0:
+            print("{}: {:d}".format(key, value))
