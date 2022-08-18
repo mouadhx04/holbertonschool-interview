87468b0cf358768b9cb671b11b1dcd97a5f0d453
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-08-18 15:22:09 +0100

    Rain!

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+-a list of non-negative integers representing the heights of walls with unit width 1
+"""
+
+
+def rain(walls):
+    """
+    Args:
+        walls - is a list of non-negative integers.
+    Returns:
+        If the list is empty return 0.
+    """
+    n = len(walls)
+    total_water = 0
+
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+
+        total_water = total_water + (min(left, right) - walls[i])
+    return total_water
