53fc430a5911aa0f6112df01414ec4502dd8dc51
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-09-20 16:20:01 +0100

    the header file

---
+++ 0x12-advanced_binary_search/search_bin.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_BIN
+#define SEARCH_BIN
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, int first, int last, int value);
+
+#endif
