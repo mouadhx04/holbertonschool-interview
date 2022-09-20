086c121184ebd4ee1245a39baaeca8f5a222fe20
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-09-20 16:44:50 +0100

    delete header.h!

--- 0x11-heap_sort/header.h
+++
@@ -1,8 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-#include <stdio.h>
-#include <stdlib.h>
-void print_array(const int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void heapify(int *array, int size, int i, int last);
-#endif
