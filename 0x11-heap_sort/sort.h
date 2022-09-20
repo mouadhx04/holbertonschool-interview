dc6ac42fb0271a6286705f1ae88129f6b4494372
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-20 16:42:19 +0100

    the header file

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, int size, int i, int last);
+#endif
