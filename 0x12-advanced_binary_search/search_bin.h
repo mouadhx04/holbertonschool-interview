0c2c0de21f65a9080a30c6ae318c004ddce51336
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-09-22 16:00:53 +0100

    Delete search_bin

--- 0x12-advanced_binary_search/search_bin.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEARCH_BIN
-#define SEARCH_BIN
-#include <stdio.h>
-#include <stdlib.h>
-
-int advanced_binary(int *array, size_t size, int value);
-int recursive_search(int *array, int first, int last, int value);
-
-#endif
