03174cfd9255791217a9f2d76db7151cd4cf5fe4
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-13 19:55:15 +0100

    Merge sort

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,122 +1,96 @@
 #include "sort.h"
 
 /**
- * sort - Sort subarrays
- * @arr: Pointer to array of integers
- * @l: Pointer to left subarray
- * @r: Pointer to right subarray
- * @start: First index of left subarray
- * @mid: Last index of left subarray
- * @end: Last index of right subarray
- *
- * Return: None
- */
-void sort(int *arr, int *l, int *r, int start, int mid, int end)
-{
-	int left_iter, right_iter, array_iter;
-	int left_size = mid - start + 1;
-	int right_size = end - mid;
-
-	left_iter = right_iter = 0;
-	array_iter = start;
-
-	while (left_iter < left_size && right_iter < right_size)
-	{
-		if (l[left_iter] < r[right_iter])
-		{
-			arr[array_iter] = l[left_iter];
-			left_iter++;
-		}
-		else
-		{
-			arr[array_iter] = r[right_iter];
-			right_iter++;
-		}
-		array_iter++;
-	}
-
-	while (left_iter < left_size)
-	{
-		arr[array_iter] = l[left_iter];
-		left_iter++;
-		array_iter++;
-	}
-
-	while (right_iter < right_size)
-	{
-		arr[array_iter] = r[right_iter];
-		right_iter++;
-		array_iter++;
-	}
-}
-
-/**
- * merge - Merge subarrays
- * @array: Pointer to array of integers
- * @start: First index of left subarray
- * @mid: Last index of left subarray
- * @end: Last index of right subarray
- *
- * Return: None
- */
-void merge(int *array, int start, int mid, int end)
-{
-	int left_iter, right_iter;
-	int left_size = mid - start + 1;
-	int right_size = end - mid;
-
-#pragma GCC diagnostic ignored "-Wvla"
-	int left[left_size];
-	int right[right_size];
-
-	for (left_iter = 0; left_iter < left_size; left_iter++)
-		left[left_iter] = array[start + left_iter];
-	for (right_iter = 0; right_iter < right_size; right_iter++)
-		right[right_iter] = array[mid + right_iter + 1];
-
-	printf("Merging...\n[left]: ");
-	print_array(left, left_size);
-	printf("[right]: ");
-	print_array(right, right_size);
-
-	sort(array, left, right, start, mid, end);
-
-	printf("[Done]: ");
-	print_array(&array[start], left_size + right_size);
-}
-
-/**
- * split_arrays - Split array into subarrays
- * @array: Pointer to array of integers
- * @start: First index of left subarray
- * @end: Last index of right subarray
- *
- * Return: None
- */
-void split_arrays(int *array, int start, int end)
-{
-	int mid = (start + end - 1) / 2;
-
-	if (start < end)
-	{
-		split_arrays(array, start, mid);
-		split_arrays(array, mid + 1, end);
-
-		merge(array, start, mid, end);
-	}
-}
-
-/**
- * merge_sort - Sort array in-place using merge sort algorithm
- * @array: Pointer to array of integers
- * @size: Number of elements in array
- *
- * Return: None
+ * merge_sort - sorts an array of integers in ascending order
+ * using the Merge sort algorithm and
+ * implementing the top-down merge sort algorithm
+ * @array: the array of integers
+ * @size: size of the array
  */
 void merge_sort(int *array, size_t size)
 {
-	if (array && size >= 2)
+	int i;
+	int *arr;
+
+	if (!array || size < 2)
+		return;
+	arr = malloc(sizeof(int) * size);
+	if (!arr)
+		return;
+	for (i = 0; i < (int)size; i++)
+		arr[i] = array[i];
+
+	split_merge(array, arr, 0, (int)(size - 1));
+	free(arr);
+}
+
+/**
+ * split_merge - splits the array into subarrays and
+ * merge sort them using the top-down merge sort algorithm
+ * @array: the array of integers
+ * @arr: the copy of array
+ * @first: index of first element
+ * @last: index of last element
+ */
+void split_merge(int *array, int *arr, int first, int last)
+{
+	int n, mid;
+
+	n = last - first + 1;
+	if (n < 2)
+		return;
+	mid = (n / 2) + first;
+	split_merge(array, arr, first, mid - 1);
+	split_merge(array, arr, mid, last);
+	merging(array, arr, first, mid, last);
+}
+
+/**
+ * merging - merges 2 sorted subarrays in an unsorted array
+ * @array: the unsorted array
+ * @arr: the temporary array that contains subarrays
+ * @first: index of first element in the first subarray
+ * @mid: index of first element in the second subarray
+ * @last: index of last element in second subarray
+ */
+void merging(int *array, int *arr, int first, int mid, int last)
+{
+	int i = first, j = mid, k = first;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array((const int *)&arr[first], mid - first);
+	printf("[right]: ");
+	print_array((const int *)&arr[mid], last - mid + 1);
+	while (i < mid && j <= last)
 	{
-		split_arrays(array, 0, size - 1);
+		if (arr[i] < arr[j])
+		{
+			array[k] = arr[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			array[k] = arr[j];
+			j++;
+			k++;
+		}
 	}
+	while (i < mid)
+	{
+		array[k] = arr[i];
+		i++;
+		k++;
+	}
+	while (j <= last)
+	{
+		array[k] = arr[j];
+		j++;
+		k++;
+	}
+	printf("[Done]: ");
+	for (i = first; i <= last; i++)
+		arr[i] = array[i];
+	print_array((const int *)&array[first], last - first + 1);
 }
