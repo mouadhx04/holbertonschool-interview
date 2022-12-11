137fb1b67101580cffeb6a2d459cb1311dede007
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-12-11 01:31:47 +0100

    making a change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Making change"""
+""" makechange challenge """
 
 
 def makeChange(coins, total):
-    """return the number of combinations"""
+    """
+    mkechange challenge
+    """
+
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    coins.sort()
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dp[i - coin] != -1:
-                dp[i] = min(dp[i - coin] + 1, dp[i])
-    return dp[total] if dp[total] != float('inf') else -1
+
+    coins.sort(reverse=True)
+    sum = 0
+    i = 0
+    counter = 0
+    num_coins = len(coins)
+    while sum < total and i < num_coins:
+        while coins[i] <= total - sum:
+            sum += coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+        i += 1
+    return -1
